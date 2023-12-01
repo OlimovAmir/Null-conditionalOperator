b5c7a8e3ff6b8e10eea2318b8db4d7208dd3ca76
olimov Amir <olimov.amir@mail.ru>
2023-12-01 11:19:26 +0300

    (myArray?.Sum() ?? 0)

--- Null.cs
+++ Null.cs
@@ -8,9 +8,16 @@ namespace Null_conditionalOperator
 {
     class Null
     {
+
+        static int[] GetArray()
+        {
+            int[] myArray = {25,85,36,15 };
+            return myArray;
+        }
         static void Main(string[] args)
         {
-
+            int[] myArray = GetArray();
+            Console.WriteLine("The sum of the Array elements " + myArray.Sum());
         }
     }
 }
